2778f521b847fa921a2acd1361cdd0290d46a975
DougWagner <digwagner@gmail.com>
2016-10-14 20:27:23 -0700

    cleaned up mov_cursor.s

--- kernel/arch/i386/mov_cursor.s
+++ kernel/arch/i386/mov_cursor.s
@@ -5,7 +5,7 @@
 mov_cursor:
     push    ebp             # save base pointer
     mov     ebp, esp        # load function stack to base pointer
-#    mov     eax, 14         
+
     mov     cx, 8[ebp]
     mov     al, 0x0F
     mov     dx, 0x3D4
@@ -13,26 +13,18 @@ mov_cursor:
     
     mov     ax, cx
     mov     dx, 0x3D5
-#    shr     eax, 8
     out     dx, al          # send lower part to VGA register
 
     mov     cx, ax
     mov     al, 0x0E
     mov     dx, 0x3D4
-    out     dx, al
+    out     dx, al          # VGA port 0x3D4
 
     mov     ax, cx
     shr     ax, 8
     mov     dx, 0x3D5
-    out     dx, al
+    out     dx, al          # send upper part to VGA register
 
-#    mov     eax, 15
-#    mov     dx, 0x3D4
-#    out     dx, eax
-#    mov     eax, 8[ebp]
-#    mov     dx, 0x3D5
-#    out     dx, eax
-#    mov     eax, 0
     pop     ebp             # restore base pointer from stack
     ret
 
