e2474df35a8dd85e01dbfe1458f03a30b2abae42
DougWagner <digwagner@gmail.com>
2016-10-21 20:16:57 -0700

    moved gdt_flush to its own file

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ cp -R --preserve=timestamps kernel/include/. sysroot/usr/include
 i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -c kernel/arch/i386/crti.s -o kernel/arch/i386/crti.o -O2 -g -ffreestanding -Wall -Wextra
 OBJ=`i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -O2 -g -ffreestanding -Wall -Wextra -print-file-name=crtbegin.o` && cp "$OBJ" kernel/arch/i386/crtbegin.o
 i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -c kernel/arch/i386/boot.s -o kernel/arch/i386/boot.o -O2 -g -ffreestanding -Wall -Wextra
+i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -c kernel/arch/i386/gdt_flush.s -o kernel/arch/i386/gdt_flush.o -O2 -g -ffreestanding -Wall -Wextra
 i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -c kernel/arch/i386/mov_cursor.s -o kernel/arch/i386/mov_cursor.o -O2 -g -ffreestanding -Wall -Wextra
 i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -c kernel/arch/i386/gdt.c -o kernel/arch/i386/gdt.o -O2 -g -ffreestanding -Wall -Wextra
 i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -c kernel/arch/i386/tty.c -o kernel/arch/i386/tty.o -std=gnu11 -O2 -g -ffreestanding -Wall -Wextra
@@ -19,7 +20,7 @@ i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/includ
 i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -c kernel/kernel/kernel.c -o kernel/kernel/kernel.o -std=gnu11 -O2 -g -ffreestanding -Wall -Wextra
 OBJ=`i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -O2 -g -ffreestanding -Wall -Wextra -print-file-name=crtend.o` && cp "$OBJ" kernel/arch/i386/crtend.o
 i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -c kernel/arch/i386/crtn.s -o kernel/arch/i386/crtn.o -O2 -g -ffreestanding -Wall -Wextra
-i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -T kernel/arch/i386/linker.ld -o DoogOS.kernel -O2 -g -ffreestanding -Wall -Wextra kernel/arch/i386/crti.o kernel/arch/i386/crtbegin.o kernel/arch/i386/boot.o kernel/arch/i386/mov_cursor.o kernel/arch/i386/gdt.o kernel/arch/i386/tty.o kernel/arch/i386/cursor.o kernel/kernel/printk.o kernel/kernel/kernel.o -nostdlib -lc -lgcc kernel/arch/i386/crtend.o kernel/arch/i386/crtn.o
+i686-elf-gcc --sysroot=/home/doug/os_project/DoogOS/sysroot -isystem=/usr/include -T kernel/arch/i386/linker.ld -o DoogOS.kernel -O2 -g -ffreestanding -Wall -Wextra kernel/arch/i386/crti.o kernel/arch/i386/crtbegin.o kernel/arch/i386/boot.o kernel/arch/i386/gdt_flush.o kernel/arch/i386/mov_cursor.o kernel/arch/i386/gdt.o kernel/arch/i386/tty.o kernel/arch/i386/cursor.o kernel/kernel/printk.o kernel/kernel/kernel.o -nostdlib -lc -lgcc kernel/arch/i386/crtend.o kernel/arch/i386/crtn.o
 echo "compilation done"
 mkdir -p sysroot/boot
 cp DoogOS.kernel sysroot/boot
--- kernel/arch/i386/boot.s
+++ kernel/arch/i386/boot.s
@@ -36,32 +36,5 @@ Hang:
 
 .size _start, . - _start
 
-# TODO: Load GDT
-.global gdt_flush
-.type gdt_flush, @function
-gdt_flush:
-    cli
-    mov     eax, 4[esp]
-    mov     2[gdt_ptr], eax
-    mov     ax, 8[esp]
-    mov     [gdt_ptr], ax
-    lgdt    [gdt_ptr]
-    jmp     0x08:complete_flush
-complete_flush:
-    mov     ax, 0x10
-    mov     ds, ax
-    mov     es, ax
-    mov     fs, ax
-    mov     gs, ax
-    mov     ss, ax
-    #sti            # this instruction is officially the offender! there is nothing wrong with the GDT! (at least I hope there isn't)
-                    # if I enable interrupts without even loading GDT, behavior will be the same
-                    # so much debugging for nothing.... looks like I need an IDT...
-    ret
-
 # TODO: Implement ISRs
 
-.section .data
-gdt_ptr:
-    .word 0
-    .long 0
