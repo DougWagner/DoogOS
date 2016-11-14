066727641a9bfa7b3153c6edf52ce88d2959629f
DougWagner <digwagner@gmail.com>
2016-11-14 00:46:23 -0800

    Primitave scancode conversion. Still needs work, but it's progress.

--- kernel/arch/i386/irq.c
+++ kernel/arch/i386/irq.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <kernel/kernel.h>
+#include <kernel/tty.h>
 
 #include "idt.h"
 
@@ -54,10 +55,48 @@ void irq_handler(struct stack_frame* frame) {
     }
 }
 
+char scancode_list[128] = {
+    0, 27, '1', '2',        // 0x00 - 0x03 -> 0x01: escape (27)
+    '3', '4', '5', '6',     // 0x04 - 0x07
+    '7', '8', '9', '0',     // 0x08 - 0x0b
+    '-', '=', 8, 9,         // 0x0c - 0x0f -> 0x0e: backspace (8), 0x0f: tab (9)
+    'q', 'w', 'e', 'r',     // 0x10 - 0x13
+    't', 'y', 'u', 'i',     // 0x14 - 0x17
+    'o', 'p', '[', ']',     // 0x18 - 0x1b
+    0, 0, 'a', 's',         // 0x1c - 0x1f -> 0x1c: enter, 0x1d: left ctrl
+    'd', 'f', 'g', 'h',     // 0x20 - 0x23
+    'j', 'k', 'l', ';',     // 0x24 - 0x27
+    '\'', '`', 0, '\\',     // 0x28 - 0x2b -> 0x2a: left shift
+    'z', 'x', 'c', 'v',     // 0x2c - 0x2f
+    'b', 'n', 'm', ',',     // 0x30 - 0x33
+    '.', '/', 0, 0,         // 0x34 - 0x37 -> 0x36: right shift, 0x37: keypad * (not implemented here yet)
+    0, ' ', 0, 0,           // 0x38 - 0x3b -> 0x38: left alt, 0x3a: capslock, 0x3b: F1
+    0, 0, 0, 0,             // 0x3c - 0x3f
+    0, 0, 0, 0,             // 0x40 - 0x43
+    0, 0, 0, 0,             // 0x44 - 0x47
+    0, 0, 0, 0,             // 0x48 - 0x4b
+    0, 0, 0, 0,             // 0x4c - 0x4f
+    0, 0, 0, 0,             // 0x50 - 0x53
+    0, 0, 0, 0,             // 0x54 - 0x57
+    0, 0, 0, 0,             // 0x58 - 0x5b
+    0, 0, 0, 0,             // 0x5c - 0x5f
+    0, 0, 0, 0,             // 0x60 - 0x63
+    0, 0, 0, 0,             // 0x64 - 0x67
+    0, 0, 0, 0,             // 0x68 - 0x6b
+    0, 0, 0, 0,             // 0x6c - 0x6f
+    0, 0, 0, 0,             // 0x70 - 0x73
+    0, 0, 0, 0,             // 0x74 - 0x77
+    0, 0, 0, 0,             // 0x78 - 0x7b
+    0, 0, 0, 0              // 0x7c - 0x7f
+};
+
 extern unsigned char get_scancode();
 
 void keyboard_handler(void) {
     unsigned char sc = get_scancode();
-    printk("%d ", sc);
+    if (sc < 128) {
+        t_putchar(scancode_list[sc]);
+    }
+    //printk("%x ", sc);
 }
 
